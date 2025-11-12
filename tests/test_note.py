84569361248f9e853ce302d426c4951370a14bd5
Radja <radjaguemoula@gmail.com>
2025-11-12 18:58:40 +0100

    red - test rating(7) returns unsuccessful

--- tests/test_note.py
+++ tests/test_note.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.note import rate_note
 
-@pytest.mark.parametrize("note",[8,9])
+@pytest.mark.parametrize("note",[7,8,9])
 def test_rate_note_returns_unsuccessful(note):
     assert rate_note(note) == "unsuccessful"
 def test_rate_10_returns_acceptable():
