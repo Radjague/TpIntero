094ac27aa46321066ee1ec1ec393e6be48593b77
Radja <radjaguemoula@gmail.com>
2025-11-12 21:57:52 +0100

    green - test rating (11) returns well

--- src/note.py
+++ src/note.py
@@ -1,6 +1,8 @@
 def rate_note(note:int) -> str:
    if note < 10:
      return "unsuccessful"
-   if note == 12:
-     return "well"
-   return "acceptable"
+   if note == 11:
+     return "acceptable"
+   if note == 10:
+     return "acceptable"
+   return "well"
