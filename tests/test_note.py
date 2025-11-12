cb01c04962b3c20b90fe279eb9f50458c9d255cd
Radja <radjaguemoula@gmail.com>
2025-11-12 21:46:26 +0100

    red - test rating (12) returns well

--- tests/test_note.py
+++ tests/test_note.py
@@ -7,3 +7,5 @@ def test_rate_note_returns_unsuccessful(note):
     assert rate_note(note) == "unsuccessful"
 def test_rate_10_returns_acceptable():
     assert rate_note(10) == "acceptable"
+def test_rate_12_returns_well():
+    assert rate_note(12) == "well"
